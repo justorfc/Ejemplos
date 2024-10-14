5bd3f6b5214c9f8e45020a3505627219dd00fc1a
justorfc <justorfc@gmail.com>
2024-10-14 04:14:51 +0000

    Modifique las label de las ecuaciones

--- graficas.py
+++ graficas.py
@@ -9,9 +9,9 @@ y3 = 6*x + 10
 
 fig, ax = plt.subplots()
 
-ax.plot(x, y1, color = "blue", label="y(x)")
-ax.plot(x,y2, color="red", label="y'(x)")
-ax.plot(x,y3, color="green", label="y''(x)")
+ax.plot(x, y1, color = "blue", label="y(x)=x**3 + 5 * x**2 + 10")
+ax.plot(x,y2, color="red", label="y'(x)=3*x**2 + 10*x")
+ax.plot(x,y3, color="green", label="y''(x)=6*x + 10")
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.legend()
